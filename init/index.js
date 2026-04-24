e27b32459ef4dd49f5e2971ab8864bf62c1245c4
Onkar Pandhare <onkarpandhare22@gmail.com>
2026-04-24 10:05:00 +0530

    remove hardcoded credentials

--- init/index.js
+++ init/index.js
@@ -1,22 +1,28 @@
-const mongoose=require('mongoose');
-const initData=require("./data.js");
-const Listing=require("../models/listing.js");
+const mongoose = require('mongoose');
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
 
- main().then((res)=>{
+const dbURL = "url";
+
+main().then(() => {
     console.log('connected to database');
-  }).catch((err)=>{
-    console.log('error connecting to database');
-  });
-async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/wonderlust');
-   
+}).catch((err) => {
+    console.log('error connecting to database', err);
+});
+
+async function main() {
+    await mongoose.connect(dbURL);
 }
 
-const initDB= async()=>{
-   await Listing.deleteMany({});
-   initData.data = initData.data.map((obj) => ({ ...obj, owner: '69e5e55d1f67954e57cae2c7' }));
-   await Listing.insertMany(initData.data);
-  console.log("data was initialized");
+const initDB = async () => {
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({ 
+        ...obj, 
+        owner: '69ea17aad4fe30e4e41e69a0'
+    }));
+    await Listing.insertMany(initData.data);
+    console.log("data was initialized");
+    mongoose.connection.close();
 }
 
 initDB();
